1ec7a162f5d62081178e7ce7e134a20069d0468f
Rahul <rahulkirti10@gmail.com>
2023-06-26 10:26:39 +0530

    resolve fixes

--- userManagement/src/main/java/com/truthyouth/commerce/config/CorsConfiguration.java
+++ userManagement/src/main/java/com/truthyouth/commerce/config/CorsConfiguration.java
@@ -1,27 +1,19 @@
 package com.truthyouth.commerce.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Component
 @Configuration
-public class CorsConfiguration extends WebMvcConfigurationSupport{
-
+@Component
+public class CorsConfiguration extends WebMvcConfigurerAdapter{
+	
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*")
-				.allowedOrigins("http://localhost:3000", "http://3.6.54.65", "http://localhost:8070/", "http://3.6.54.65:8070/")
-				.allowCredentials(true)
-				.exposedHeaders(HttpHeaders.AUTHORIZATION);
-	}
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/swagger-ui/index.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+				.allowedOrigins("http://localhost:3000", "http://3.6.54.65", "http://localhost:8070/swagger-ui/index.html", "http://3.6.54.65:8070/")
+				.allowCredentials(true);
 	}
 }
